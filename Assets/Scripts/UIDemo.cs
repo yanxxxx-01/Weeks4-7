3b4711479a6a7dbcec41ca824bbcd36df21f2d06
Yanxxx <060509cindy@gmail.com>
2026-02-02 02:52:20 -0500

    Week 5 Preview

--- Assets/Scripts/UIDemo.cs
+++ Assets/Scripts/UIDemo.cs
@@ -1,19 +1,34 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class UIDemo : MonoBehaviour
 {
     SpriteRenderer sr;
+    public Image sImage; //the image component reference
+    public int howManyClicks = 0;
+    public TextMeshProUGUI score; //the text component reference
+    public Slider slider;
+    public TextMeshProUGUI sliderValue;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+
+        slider.wholeNumbers = true;
+        slider.minValue = -3f;
+        slider.maxValue = 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Keyboard.current.anyKey.wasPressedThisFrame == true){
+        sliderValue.text = slider.value.ToString();
+
+        if (Keyboard.current.anyKey.wasPressedThisFrame == true){
 
             ChangeColour();
         }
@@ -23,11 +38,18 @@ public class UIDemo : MonoBehaviour
     public void ChangeColour()
     {
         sr.color = Random.ColorHSV();
-
+        sImage.color = sr.color;
     }
 
     public void ScaleBig(float scaleAmount)
     {
         transform.localScale = Vector3.one * scaleAmount;
+    }
+
+    public void CountClicks()
+    {
+        howManyClicks++;
+        score.text = howManyClicks.ToString();
+        Debug.Log("Button clicked " + howManyClicks + " times.");
     }
 }
